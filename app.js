3dfd8615dc75245a492a9bce0754a56cd1e3bec5
xfuzzylogic <151855851+xfuzzylogic@users.noreply.github.com>
2023-12-20 15:04:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'README.md'));
 });
 
 app.get('/getUsersInfo', async (req, res) => {
